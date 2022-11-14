7a6455bdfdc3f30cb17107c41bbc840371f5c0ed
nehemias-bit <nemicruz003@gmail.com>
2022-11-14 12:26:02 -0500

    working containsduplicate

---
+++ arrays-and-hashing/ContainsDuplicates.java
@@ -0,0 +1,24 @@
+import java.util.HashSet;
+
+public class ContainsDuplicates {
+  public static void main(String[] args) {
+
+  }
+
+  // Time complexity: Big O(n)
+  // Space complexity: Big O(n)
+  public static boolean containsDuplicates(int[] nums) {
+
+    HashSet<Integer> numsSet = new HashSet<Integer>();
+    for (int i = 0; i < nums.length; i++) {
+      numsSet.add(nums[i]);
+    }
+
+    if (numsSet.size() < nums.length) {
+      return true;
+    }
+    return false;
+
+  }
+
+}
